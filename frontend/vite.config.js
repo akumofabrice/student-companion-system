85580bfee2dad6dda0c9cfbcf3fd82b02e059949
Fabrice Akumo <akumofabrice7@gmail.com>
2026-07-07 01:18:04 -0400

    trying to fix cors issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://studentmanager-usv7.onrender.com',
         changeOrigin: true,
       },
     },
